6481ec16a647dd0866f7e9ad6ff2d504a73b5115
Garrett Dancik <garrett.dancik@gmail.com>
2019-10-14 14:51:35 -0400

    Update

---
+++ hello.java
@@ -0,0 +1,19 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+
+        String firstName = "Elizabeth";
+        String lastName = "Jones";
+	
+        System.out.println("Hello Class!");
+
+
+
+    }
+}
+
+
+`
